d19e96478916e9706f8d8f3704750013f97f0cc9
MAC M1 <m.isakov.jcc@gmail.com>
2022-12-28 13:49:47 +0600

    heroku

--- mybot.py
+++ mybot.py
@@ -1,32 +1,26 @@
 import telebot
 import pygsheets
-from telebot import types
-
-gc = pygsheets.authorize(service_file='creds.json')
-
-sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1SmBSeig_GSEftIT6Zpqx8dhWP4Bf7CjVnb0VJw48feY/edit#gid=367067341')
-sheet = sh.sheet1
-
+import time
 TOKEN = '5719851205:AAF-g4aYF6_IfZyDLYO3RcLbPxyXRJxIlKM'
 bot = telebot.TeleBot(TOKEN)
 
-btn1 = types.InlineKeyboardButton('Количество заявок')
-inline_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-inline_keyboard.add(btn1)
 
-
-@bot.message_handler(commands=['start', ])
+@bot.message_handler(commands=['AgIAAxkBAAEBf95jjHTrpS-9g4ZnKak8IW5JRj0kdQACHgADZ8j4Ipl0YqyHEWzpKwQ', ])
 def start_message(message):
-    chat_id = message.chat.id
-    msg = bot.send_message(chat_id, f'Вас приветствует бот!', reply_markup=inline_keyboard)
-    bot.register_next_step_handler(msg, inline)
+    while True:
+        try:
+            chat_id = message.chat.id
+            gc = pygsheets.authorize(service_file='creds.json')
+            sh = gc.open_by_url(
+                'https://docs.google.com/spreadsheets/d/1SmBSeig_GSEftIT6Zpqx8dhWP4Bf7CjVnb0VJw48feY/edit#gid=367067341')
+            sheet = sh.sheet1
+            bot.send_message(chat_id, f'Добуштардын саны/Количество голосов: {sheet.rows} \n'
+                                      f'ЗаЯпошки үчүн добуштарды билүү үчүн, ушул шилтемеге кириңиз https://zayaposhki.stars.kg/')
+
+            time.sleep(1800)
+        except:
+            pass
 
 
-@bot.callback_query_handler(func=lambda c: True)
-def inline(c):
-    chat_id = c.chat.id
-    msg = bot.send_message(chat_id, f'{sheet.rows}')
-    bot.register_next_step_handler(msg, inline)
-
-
-bot.polling()
+if __name__ == '__main__':
+    bot.polling()
